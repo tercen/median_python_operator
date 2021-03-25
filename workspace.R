e0df60b8b2ea5b91e225b8186ce9056a9b6f19b5
Alexandre Maurel <alexandre.maurel@gmail.com>
2021-03-25 09:49:31 +0000

    conda env

--- workspace.R
+++ workspace.R
@@ -2,10 +2,13 @@ library(tercen)
 library(dplyr)
 library(reticulate)
 
-# renv::init()
-# renv::use_python()
-# 
-# reticulate::py_install("pandas")
+renv::init()
+renv::use_python(type="conda")
+
+reticulate:::py_install_method_detect("tesdt")
+reticulate::py_install("pandas", method = "conda")
+
+renv::snapshot()
 
 # http://127.0.0.1:5402/admin/w/99ca4c32889264fd1c01b57bde00c4cd/ds/8035a1c5-cd31-41b9-9ddb-e6babc5ebf9d
 options("tercen.workflowId" = "99ca4c32889264fd1c01b57bde00c4cd")
